7f735a5d5984874e1c256cbaafec8a295a9a31c1
jamariod <j25ghs99@aol.com>
2020-08-11 23:13:42 -0400

    Add files via upload

---
+++ FizzBuzz.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+// FizzBuzz Algorithm
+var oneThousandNumbers = [Int]()
+for i in 1...1000 {
+    oneThousandNumbers.append(i)
+}
+for num in oneThousandNumbers {
+    if num % 15 == 0 {
+        print("\(num) fizzbuzz")
+    } else if num % 3 == 0 {
+        print("\(num) fizz")
+    } else if num % 5 == 0 {
+        print("\(num) buzz")
+    } else {
+    print(num)
+    }
+}
